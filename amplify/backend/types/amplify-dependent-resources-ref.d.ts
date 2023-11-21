ffd6c5ba3320f7086e124a77d4ce486c0a69f97b
zhangshouyin <zhangshouyin_good@163.com>
2023-11-21 20:22:54 +0800

    添加 react 界面

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,20 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "function": {
+    "S3Trigger32b962a3": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "s3266ab626": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
